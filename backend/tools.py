9e7f67a07a0dcd001a30092a9f5202aea112317d
Nik <nikhilrr2311@gmail.com>
2026-09-02 22:31:25 +0200

    feat:arxiv tool call for the model

--- backend/tools.py
+++ backend/tools.py
@@ -12,5 +12,14 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 wikipedia = WikipediaQueryRun(
     api_wrappper=WikipediaAPIWrapper,
     top_k_results=2,
-    doc_content_chars_max=400,
+    doc_content_chars_max=4000,
+)
+
+#ArcXiv 
+
+arxiv = ArxivQueryRun(
+    arxiv_wrapper = ArxivAPIWrapper(
+    top_k_results=2,
+    doc_content_chars_mnax=4000,
+    )
 )
