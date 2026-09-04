f89c4c6a10af68eac02d40e2cf4b6e2d715e37cd
Nik <nikhilrr2311@gmail.com>
2026-09-04 21:37:00 +0200

    feat: add the tool call for retrieval

--- backend/tools.py
+++ backend/tools.py
@@ -7,6 +7,8 @@ from langchain_community.utilities import (
     WikipediaAPIWrapper,
 )
 from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.tools import tool
+from backend.vector_store import get_vector_store
 
 # wikipedia
 wikipedia = WikipediaQueryRun(
@@ -36,3 +38,31 @@ tools = [
   arxiv,
   tavily,
 ]
+
+@tool
+def retrieve_knowledge(query : str) -> str:
+    """
+    Search the knowledge base for the information relavent to users question
+    Use this tool when the answer may be found in user's document
+    """
+
+    vector_store = get_vector_store()
+
+    documents = vector_store.similarity_search(
+        query,
+        k=4,
+    )
+
+    if not documents:
+        return "No relevant information was found in knowledge base"
+
+    context = []
+
+    for document in documents:
+
+        context.append(
+            f"Source:{document.metadata.get('source', 'unknown')}\n"
+            f"Content:{document.page_content}"
+        )
+
+        return "\n\n---\n\n".join(context)
