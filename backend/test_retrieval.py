ab06951ed392c7f00c320ed102792bd5ef1a508b
Nik <nikhilrr2311@gmail.com>
2026-09-05 11:18:14 +0200

    feat: test the retreival

---
+++ backend/test_retrieval.py
@@ -0,0 +1,16 @@
+from backend.vector_store import get_vector_store
+
+vector_store = get_vector_store()
+
+results = vector_store.get_similarity_search(
+    "what is Langgraph",
+    k=4,
+)
+
+for i, document in enumerate(results, start=1):
+    print(f"\n--- Result {i} ---")
+
+    print("Source:", document.metadata.get("source"))
+
+    print("Content:")
+    print(document.page_content)
