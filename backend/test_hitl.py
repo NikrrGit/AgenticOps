9608fa6b0cde89ee34f8bd03442d4e3481ba92c3
Nik <nikhilrr2311@gmail.com>
2026-09-06 19:12:08 +0200

    test: test file for  hitl

---
+++ backend/test_hitl.py
@@ -0,0 +1,10 @@
+from langgraph.types import Command
+from backend.chatbot   import chatbot
+
+thread_id = "hitl-test-1"
+
+config = {
+    "configurable" : {
+        "thread_id" : thread_id,
+    }
+}
